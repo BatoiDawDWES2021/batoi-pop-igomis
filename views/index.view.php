2354eed6fabb2a93fc01ceb2a0ef1751b4e3507b
igomis <igomis@cipfpbatoi.es>
2021-09-25 23:14:14 +0200

    1.1 branch

---
+++ views/index.view.php
@@ -0,0 +1,6 @@
+<?php
+    require_once('partials/head.view.php');
+    require_once('partials/navigation.view.php');
+    require_once('partials/header.view.php');
+    require_once('partials/section.view.php');
+    require_once('partials/footer.view.php');
